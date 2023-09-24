7c5cee5f2d606803526fbde7a041dc4256737ba2
lnrd0 <leonardotaiida@gmail.com>
2023-09-24 15:26:47 +0000

    Update 05-config-runners.sh

--- install/05-config-runners.sh
+++ install/05-config-runners.sh
@@ -2,7 +2,7 @@ docker exec -it runner-docker \
 gitlab-runner register -n \
   --url https://gitlab.com/ \
   --registration-token GR1348941yKKZ9nu1VoKNMVtQERUG \
-  --clone-url http://IP \
+  --clone-url https://gitlab.com/ \
   --executor docker \
   --docker-image "docker:latest" \
   --docker-privileged
